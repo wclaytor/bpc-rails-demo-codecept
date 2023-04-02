9677fcfda91ad1721730462017a1ed102620ad8a
Bill Claytor <bill.claytor@active911.com>
2023-04-02 13:37:06 -0700

    Updated homepagePage with elements and verification Updated homepage_test to use homepagePage Removed old Homepage.js file

--- tests/homepage/homepage_test.js
+++ tests/homepage/homepage_test.js
@@ -1,6 +1,7 @@
 Feature('Homepage');
 
-Scenario('Verify page elements',  ({ I }) => {
+Scenario('Verify page elements',  ({ I, homepagePage }) => {
     I.amOnPage('http://demo.billclaytor.com');
     I.see('Developer Bookshelf');
+    homepagePage.verifyPageElements();
 });
